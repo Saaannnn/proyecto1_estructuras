c1ec6bc0cf3c4e0fb4b8444996ab33de7ffba26a
ale <alessandroperezcasado@gmail.com>
2025-05-03 16:35:23 -0400

    V1.0

---
+++ prueba1.py
@@ -0,0 +1,9 @@
+import colorama
+from colorama import *
+
+a=int(input("Ingrese el primer número: "))
+b=int(input("Ingrese el segundo número: "))
+
+x=a+b
+
+print(Fore.BLUE + "La suma de los números es: ", x)
